a877ce31f8236aee0f4c3208e2ea801ab858c8ba
saba syed <sabaa9018@gmail.com>
2023-10-25 14:15:33 +0530

    submitting, displaying todos

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import TodoItem from "./TodoItem";
 
 export default function Todo() {
-  const [todo,setTodo] = useState("");
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <div>
-      <form>
-        <input onChange={(e)=>setTodo(e.target.value)} value={todo} type="text" />
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        />
+        <button type="submit">Add</button>
       </form>
+      {todos.map((item) => (
+        <TodoItem key={item} item={item}/>
+      ))}
+      {/* {console.log(todos)} */}
     </div>
   );
 }
