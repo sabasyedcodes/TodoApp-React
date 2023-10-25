5fff65b6f5f79b24736b5430dfe4891a3a0a88ad
saba syed <sabaa9018@gmail.com>
2023-10-25 14:34:28 +0530

    added css style sheet

--- todoapp/src/components/InlineComponent.jsx
+++ todoapp/src/components/InlineComponent.jsx
@@ -1,8 +1,8 @@
-const header ={color:"blue",fontSize:"140px"}
+import "../css/style.css";
 export default function InlineComponent() {
   return (
     <div>
-      <h1 style={header}>Inline Component</h1>
+      <h1 className="header">Inline Component</h1>
     </div>
   );
 }
