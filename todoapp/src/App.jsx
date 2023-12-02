4d2f263efdbbaabac5638003ea4c0f3fa3c4af13
saba syed <sabaa9018@gmail.com>
2023-12-02 11:18:13 +0530

    styling of the todo items and todolist

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,10 +1,11 @@
-
+import Header from "./components/Header";
 import Todo from "./components/Todo";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+     <Header />
      <Todo />
     </div>
   );
--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,28 +1,14 @@
 import React, { useState } from "react";
-import TodoItem from "./Todoitem";
+
+import Form from "./form";
+import TodoList from "./TodoList";
 
 export default function Todo() {
-  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
-          type="text"
-        />
-        <button type="submit">Add</button>
-      </form>
-      {todos.map((item) => (
-        <TodoItem key={item} item={item} />
-      ))}
-      {/* {console.log(todos)} */}
+      <Form todos={todos} setTodos={setTodos}/>
+      <TodoList todos={todos} />
     </div>
   );
 }
