26e60792e822c187c93d701eaa17200e6aa34bbc
saba syed <sabaa9018@gmail.com>
2023-12-02 13:30:37 +0530

    strike feature added with react

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -5,12 +5,21 @@ export default function TodoItem({ item,todos,setTodos}) {
     setTodos(todos.filter((todo) => todo !== item));
 
   }
+  function handleClick(name){
+    const newArray = todos.map((todo)=>todo.name===name?{...todo,done:!todo.done}: todo)
+    setTodos(newArray)
+    
+  }
+  const className = item.done ? styles.completed : ""
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={className} onClick={()=>handleClick(item.name)}>{item.name}</span>
         <span>
-          <button onClick={()=>handleDelete(item)} className={styles.deleteButton}>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
             x
           </button>
         </span>
